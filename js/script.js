18bd92861682b9c0ef12e69edaac012830b25552
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-07 15:53:43 +0300

    add okruglenie

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ document.getElementById('del').onclick = function() {
     var num2 = document.getElementById('V').value;
     var result = num1 / num2;
 
-    document.getElementById('out').innerHTML = 'плотность = ' + result;
+    document.getElementById('out').innerHTML = 'плотность = ' + result + 'кг/м в кубе';
+};
+
+document.getElementById('res2').onclick = function() {
+    var num1 = document.getElementById('m').value;
+    var num2 = document.getElementById('V').value;
+    var result = num1 / num2;
+    var result2 = result.toFixed();
+
+    document.getElementById('out2').innerHTML = result2;
 };
